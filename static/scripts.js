bee5fdbd911daa8a6b638c8fa7a553eed9f08dc4
Dement0 <ilkeer.ozgur@gmail.com>
2018-07-29 20:18:23 +0200

    Added remaining functions

--- static/scripts.js
+++ static/scripts.js
@@ -174,5 +174,63 @@ function removeMarkers()
 // Search database for typeahead's suggestions
 function search(query, syncResults, asyncResults)
 {
-  // Get places matching query
+  // Get places matching query (asynchronously)
+  let parameters = {
+    q: query
+  };
+  $.getJSON("/search", parameters, function(data, textStatus, jqXHR) {
+    // Call typeahead's callback with search results (i.e., places)
+    asyncResults(data);
+  });
 }
+
+// Show info window at marker with content
+function showInfo(marker, content)
+{
+  // Start div
+  let div = "<div id='info'>";
+  if (typeof(content) == "undefined")
+  {
+    //http://www.ajaxload.info
+    div += "<img alt='loading' src='static/ajax-loader.gif'/>";
+  }
+  else
+  {
+    div += content;
+  }
+
+  // End div
+  div += "</div>";
+
+  // Set info window's content
+  info.setContent(div);
+
+  // Open info window (if not already open)
+  info.open(map, marker);
+}
+
+// Update UI's markers
+function update()
+{
+  // Get map's bounds
+  let bounds = map.getBounds();
+  let ne = bounds.getNorthEast();
+  let sw = bounds.getSotuhWest();
+
+  // Get places within bounds (asynchronously)
+  let parameters = {
+    ne: `${ne.lat()},${ne.lng()}`,
+    q: $("#q").val(),
+    sw: `${sw.lat()},${sw.lng()}`
+  };
+  $.getJSON("/update", parameters, function(data, textStatus, jqXHR) {
+    // Remove old markers from map
+    removeMarkers();
+
+    // Add new markers to map
+    for (let i = 0; i < data.length; i++)
+    {
+      addMarker(data[i]);
+    }
+  });
+};
